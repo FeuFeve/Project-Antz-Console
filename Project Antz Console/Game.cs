929504d67555a4ee5f0aa3a3a4bd0c6bf8884e6e
FeuFeve <clement-potin@outlook.com>
2020-04-17 02:58:50 +0200

    Added 'CommandManager'.

--- Project Antz Console/Game.cs
+++ Project Antz Console/Game.cs
@@ -8,12 +8,14 @@ namespace Project_Antz_Console
         {
             Console.WriteLine("From Game: Hello world!");
             
-            Server server1 = new Server("S1", 10);
-            server1.DisplayPlayers();
+            // Server server1 = new Server("S1", 10);
+            // server1.DisplayPlayers();
+            //
+            // Player player = new Player("FeuFeve");
+            // server1.AddPlayer(player);
+            // server1.DisplayPlayers();
             
-            Player player = new Player("FeuFeve");
-            server1.AddPlayer(player);
-            server1.DisplayPlayers();
+            CommandManager.WaitForNextCommand();
         }
     }
 }
